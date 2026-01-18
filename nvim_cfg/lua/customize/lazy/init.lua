7594bbe357cf7539ea93e0145ff0426d78949859
tzyyshyanglin <49217577+tzyyshyanglin@users.noreply.github.com>
2026-01-18 16:33:57 +0800

    Create init.lua

---
+++ nvim_cfg/lua/customize/lazy/init.lua
@@ -0,0 +1,6 @@
+return {
+  -- Treesitter for syntax highlighting
+  { 'ThePrimeagen/vim-be-good' }, -- For VIM motion training
+  { 'mbbill/undotree' }, -- TODO: Learn to how to effectively use undotree!!
+  { 'tpope/vim-fugitive' }, -- TODO: Learn how to effectivley use git in nvim
+}
