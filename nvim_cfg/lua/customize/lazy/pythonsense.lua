e0ada4f55124b2682b06deef25f3564bbca8a0c8
tzyyshyanglin <49217577+tzyyshyanglin@users.noreply.github.com>
2026-01-18 16:38:12 +0800

    Create pythonsense.lua

---
+++ nvim_cfg/lua/customize/lazy/pythonsense.lua
@@ -0,0 +1,9 @@
+return {
+  { -- For python function and class textobjects
+    'jeetsukumaran/vim-pythonsense',
+
+  },
+  { -- For python indent textobjects in VIM
+    'michaeljsmith/vim-indent-object',
+  },
+}
