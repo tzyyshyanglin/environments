f7fc32725dc2eb902ec7ccda15fe8507f5a133d0
tzyyshyanglin <49217577+tzyyshyanglin@users.noreply.github.com>
2026-01-18 16:38:53 +0800

    Create telescope.lua

---
+++ nvim_cfg/lua/customize/lazy/telescope.lua
@@ -0,0 +1,50 @@
+return { -- Fuzzy finder telescope
+  'nvim-telescope/telescope.nvim',
+  branch = '0.1.x',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-telescope/telescope-ui-select.nvim',
+    'nvim-tree/nvim-web-devicons',
+    'nvim-telescope/telescope-fzf-native.nvim',
+  },
+  event = 'VeryLazy',
+  config = function()
+    local telescope = require 'telescope'
+    local ts_builtin = require 'telescope.builtin'
+    telescope.setup {
+      defaults = {
+        file_ignore_patterns = {
+          'node_modules/*',
+          -- Python related directories
+          '__pycache__/*',
+          'site%-packages/*',
+        },
+      },
+    }
+
+    -- Adding additional telescope plugins
+    pcall(telescope.load_extension, 'fzf')
+    pcall(telescope.load_extension, 'ui-select')
+
+    -- Setting up the key bindings (with which-key)
+    local wk = require 'which-key'
+    wk.add {
+      { '<leader>s', group = 'search', mode = 'n', icon = '' },
+      { '<leader>sf', ts_builtin.find_files, desc = '[S]earch [F]ile', icon = '' },
+      --{ '<leader>st', ts_builtin.git_files, desc = '[S]earch git-[T]racked', icon = '' },
+      { '<leader>sh', ts_builtin.help_tags, desc = '[S]earch [H]elp', icon = '' },
+      { '<leader>sw', ts_builtin.grep_string, desc = '[S]earch current [W]ord' },
+      { '<leader>sg', ts_builtin.live_grep, desc = '[S]earch by [G]rep' },
+      { '<leader>sr', ts_builtin.resume, desc = '[S]earch [R]esume' },
+      { '<leader>s.', ts_builtin.oldfiles, desc = '[S]earch Recent Files ("." for repeat)' },
+      { '<leader>sb', ts_builtin.buffers, desc = '[S]earch [B]uffers' },
+      { '<leader>sc', ts_builtin.treesitter, desc = '[S]earch [C]ode with Treesitter' },
+      -- Search key maps is handled by telescope (for all key maps without explicit description )
+      { '<leader>sk', ts_builtin.keymaps, desc = '[S]earch [K]eymaps', icon = '' },
+      -- Spell checking is considered a code actions
+      --{ '<leader>as', ts_builtin.spell_suggest, desc= '[A]ction [S]pell sugggestion', icon=""}
+      -- notification history from nvim-notify
+      { '<leader>sn', function() require('telescope').extensions.notify.notify() end, desc = '[S]earch [N]otifications', icon = '' },
+    }
+  end,
+}
